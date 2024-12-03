44f9e7741dd1c3abf1b39cfd060003402e3970da
meruuuuooo <merudeibitto@gmail.com>
2024-12-04 01:11:29 +0800

    Add migrations for Products, Inventories, Categories, Sales, SalesDetails, and RestockingHistories tables

---
+++ database/migrations/2024_12_02_011716_create_sales_details_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sale_id')->constrained('sales');
+            $table->foreignId('product_id')->constrained('products');
+            $table->integer('quantity_sold')->default(0);
+            $table->decimal('line_total', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales_details');
+    }
+};
