4271ba86a0533168bf04324fadc0f72dfcd19967
meruuuuooo <merudeibitto@gmail.com>
2024-12-10 23:57:36 +0800

    Comment out UserSeeder and enable CategorySeeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,7 +25,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             // RoleSeeder::class,
-            UserSeeder::class,
+            // UserSeeder::class,
+            CategorySeeder::class,
         ]);
     }
 }
