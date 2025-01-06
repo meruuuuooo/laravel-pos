46075382e60805549b33ec95429056bc7373f502
Mel David Mailem <mailem.meldavid@gmail.com>
2025-01-06 21:54:16 +0800

    Improve styling of Sales Report page by adding padding to the main container

--- resources/js/Pages/Sales/Index.jsx
+++ resources/js/Pages/Sales/Index.jsx
@@ -115,7 +115,7 @@ export default function Index({
             <Head title="Sales Report" />
             <div className="py-6">
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div className="h-full w-full rounded-lg bg-white shadow">
+                    <div className="h-full w-full rounded-lg bg-white p-6 shadow">
                         <div className="rounded-t-lg border-b p-4">
                             <div className="mb-8 flex items-center justify-between">
                                 <div>
